64757be8f24683a9fe46d76cc84021d036880a24
amithamurali <murali.a@husky.neu.edu>
2017-04-17 19:54:05 -0400

    Event Hub Listener

---
+++ EventListener/EventListener/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Azure.EventHubs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventListener
+{
+    class Program
+    {
+        private static EventHubClient eventHubClient;
+        private const string EhConnectionString = "{Event Hubs connection string}";
+        private const string EhEntityPath = "{mytollentryeventhub}";
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
+            // Typically, the connection string should have the entity path in it, but for the sake of this simple scenario
+            // we are using the connection string from the namespace.
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EhConnectionString)
+            {
+                EntityPath = EhEntityPath
+            };
+
+            eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            await SendMessagesToEventHub(100);
+
+            await eventHubClient.CloseAsync();
+
+            Console.WriteLine("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        // Creates an Event Hub client and sends 100 messages to the event hub.
+        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        {
+            for (var i = 0; i < numMessagesToSend; i++)
+            {
+                try
+                {
+                    var message = $"Message {i}";
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                await Task.Delay(10);
+            }
+
+            Console.WriteLine($"{numMessagesToSend} messages sent.");
+        }
+    }
+}
