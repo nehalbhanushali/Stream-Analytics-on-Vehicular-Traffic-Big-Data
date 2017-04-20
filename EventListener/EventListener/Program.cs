2b7512b54a6aab148111acaaa8b138486a45bebf
amithamurali <murali.a@husky.neu.edu>
2017-04-20 12:05:13 -0400

    Add files via upload

--- EventListener/EventListener/Program.cs
+++ EventListener/EventListener/Program.cs
@@ -2,20 +2,32 @@
 using Microsoft.Azure.EventHubs;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace EventListener
 {
     class Program
     {
         private static EventHubClient eventHubClient;
-        private const string EhConnectionString = "{Event Hubs connection string}";
-        private const string EhEntityPath = "{mytollentryeventhub}";
+        private const string EhConnectionString = "Endpoint=sb://matrix4entry.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ZnAA8gP+43y/7oPeIX4z7jKNcOHyh0RA6PB7GtNAdws=";
+        private const string EhEntityPath = "mytollentryeventhub";
+        private static List<VehicleItem> items = new List<VehicleItem>();
 
         static void Main(string[] args)
         {
             MainAsync(args).GetAwaiter().GetResult();
         }
 
+        public static void LoadJson()
+        {
+            using (StreamReader r = File.OpenText(@"C:\Amitha NEU\Engg Big Data\Final\SAJ\EntryStreamInput.json"))
+            {
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<VehicleItem>>(json);               
+            }
+        }
         private static async Task MainAsync(string[] args)
         {
             // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
@@ -28,7 +40,8 @@ namespace EventListener
 
             eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
 
-            await SendMessagesToEventHub(100);
+            LoadJson();
+            await SendJsonObjectsToEventHub();
 
             await eventHubClient.CloseAsync();
 
@@ -37,25 +50,67 @@ namespace EventListener
         }
 
         // Creates an Event Hub client and sends 100 messages to the event hub.
-        private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        private static async Task SendJsonObjectsToEventHub()
         {
-            for (var i = 0; i < numMessagesToSend; i++)
-            {
+            foreach (VehicleItem item in items)
+            {              
+                      
                 try
                 {
-                    var message = $"Message {i}";
-                    Console.WriteLine($"Sending message: {message}");
-                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                    Console.WriteLine(item.Manufacturer);
+                    Console.WriteLine(item.MakeAndModel);
+                    Console.WriteLine(item.LicensePlatePK);
+                    Console.WriteLine(item.LicensePlateRK);
+                    var jsonItem = "{ \"" + "Manufacturer" + "\" : \"" + item.Manufacturer + "\" ," +
+                        " \"" + "MakeAndModel" + "\" : \"" + item.MakeAndModel + "\"  , " +
+                        "\"" + "licenseplatePK" + "\" : \"" + item.LicensePlatePK + "\" ," +
+                        " \"" + "licenseplateRK" + "\" : \"" + item.LicensePlateRK + "\"}";
+                    
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(jsonItem)));
+                  
                 }
                 catch (Exception exception)
                 {
                     Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
                 }
 
-                await Task.Delay(10);
+                //await Task.Delay(10);
             }
 
-            Console.WriteLine($"{numMessagesToSend} messages sent.");
         }
+        //private static async Task SendMessagesToEventHub(int numMessagesToSend)
+        //{
+        //    for (var i = 0; i < numMessagesToSend; i++)
+        //    {
+        //        try
+        //        {
+        //            // var message = $"Message {i}";
+
+        //            var j = i % 10;
+        //            var Manufacturer = "Manufacturer";
+        //            var ManufacturerValue = "Chevy";
+        //            var MakeAndModel = "MakeAndModelValue";
+        //            var MakeAndModelValue = "Bolt";
+        //            var licenseplatePK = "licenseplatePK";
+        //            var licenseplatePKValue = "200" + j;
+        //            var licenseplateRK = "licenseplateRK";
+        //            var licenseplateRKValue = "85234" + i;
+        //            var json = "{ \"" + Manufacturer + "\" : \"" + ManufacturerValue + "\" , \"" + MakeAndModel + "\" : \"" + MakeAndModelValue + "\"  , \"" + licenseplatePK + "\" : \"" + licenseplatePKValue + "\" , \"" + licenseplateRK + "\" : \"" + licenseplateRKValue + "\"}";
+        //            //  Console.WriteLine($"Sending message: {json}");
+
+
+        //            // await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(json)));
+
+        //        }
+        //        catch (Exception exception)
+        //        {
+        //            Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+        //        }
+
+        //        await Task.Delay(10);
+        //    }
+
+        //    Console.WriteLine($"{numMessagesToSend} messages sent.");
+        //}
     }
 }
---
+++ EventListener/EventListener/vehicleItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventListener
+{
+    class VehicleItem
+    {
+        public string Manufacturer;
+        public string MakeAndModel;        
+        public string LicensePlatePK;
+        public string LicensePlateRK;
+        public DateTime datetime;
+
+    }
+}
