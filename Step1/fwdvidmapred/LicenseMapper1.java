bce4f6ec6df92525fbb11cd805f09d2a35dff6b8
Nehal Bhanushali <Nehal Bhanushali>
2017-04-19 21:24:40 -0400

    java mapreduce to assign make model added in step 1

---
+++ Step1/fwdvidmapred/LicenseMapper1.java
@@ -0,0 +1,63 @@
+
+// Solution - As discussed by the fellow classmate in class
+// modified by nehal
+
+package U.CC;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+import java.util.*;
+import java.lang.StringBuilder;
+
+ /*
+  * @nehal
+  *
+  *
+  */
+ public class LicenseMapper1 extends MapReduceBase implements Mapper<LongWritable, Text, Text, Text> {
+
+   public void map(LongWritable key, Text value,
+                   OutputCollector output, Reporter reporter) throws IOException
+{
+     // Prepare the input data.
+    String license = value.toString();
+    //System.out.println("< "+key.toString()+" > "+"< "+license+" >");
+    String licenseMap[] = license.split(" ");
+    //System.out.println("length >>"+licenseMap.length);
+    // we are not interested in people who have no friends at all
+    if(licenseMap.length == 4){
+
+    String plate_number = licenseMap[1];
+    String confidence = licenseMap[3];
+	
+	output.collect(new Text(plate_number), new Text(confidence));
+
+   // int indexOfcrime = friend.indexOf("#");
+    // emmit reverse friendship only if part2 is a person and not a crime
+	/*
+    if(indexOfcrime == -1){
+      output.collect(new Text(person), new Text(friend));
+      output.collect(new Text(friend), new Text(person));
+    }else{
+      output.collect(new Text(person), new Text("list"));
+    }
+*/
+
+}else{
+  System.out.println("< "+key.toString()+" > "+"< "+license+" >");
+  return;
+}
+
+   }
+
+ }
---
+++ Step1/fwdvidmapred/LicenseReducer1.java
@@ -0,0 +1,173 @@
+// Solution - As discussed by the fellow classmate in class
+// modified by nehal
+
+package U.CC;
+
+ import java.io.IOException;
+ import java.util.Iterator;
+
+ import org.apache.hadoop.io.IntWritable;
+ import org.apache.hadoop.io.Text;
+ import org.apache.hadoop.io.WritableComparable;
+ import org.apache.hadoop.mapred.MapReduceBase;
+ import org.apache.hadoop.mapred.OutputCollector;
+ import org.apache.hadoop.mapred.Reducer;
+ import org.apache.hadoop.mapred.Reporter;
+ import java.lang.StringBuilder;
+ import java.util.*;
+ 
+ 
+ 
+ //reading from csv imports
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.nio.charset.StandardCharsets; 
+import java.nio.file.Files; 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList; 
+import java.util.List;
+
+
+ public class LicenseReducer1 extends MapReduceBase implements Reducer<Text, Text, Text, Text>
+{
+   private static List<VehicleSpecies> vehiclesSpeciesList = readModelsFromCSV("CAVtags.txt");
+
+   public void reduce(Text key, Iterator<Text> values,
+                      OutputCollector<Text, Text> output, Reporter reporter) throws IOException {
+
+     String plate_number = key.toString();
+	 //System.out.println(">>>>>>>>>>>>>>>> "+plate_number);
+
+	 
+	 String finalOutput = "{ \"LicensePlateRK\" : \""+plate_number+"\" , "+getVehicleSpecies(plate_number)+" }";
+	 
+	 
+	 System.out.println(finalOutput);
+	 
+	 output.collect(new Text(plate_number+" : "), new Text(finalOutput) );
+
+  }
+  
+  public static class VehicleSpecies {
+	    private int year;
+		private String manufacturer, makeAndModel, exhaustStandard, fuelType, decalColour;
+		//private double mpg;
+
+		public VehicleSpecies(int year, String manufacturer,String makeAndModel, String exhaustStandard ,String fuelType ,String decalColour) {
+			super();
+			
+			this.year = year;
+			this.manufacturer = manufacturer;
+			this.makeAndModel = makeAndModel;
+			this.exhaustStandard = exhaustStandard;
+			this.fuelType = fuelType;
+			this.decalColour = decalColour;
+			//this.mpg = mpg;
+		}
+
+		public int getYear() {
+			return year;
+		}
+
+		public String getManufacturer() {
+			return manufacturer;
+		}
+
+		public String getMakeAndModel() {
+			return makeAndModel;
+		}
+		
+		public String getExhaustStandard() {
+			return exhaustStandard;
+		}
+		
+		public String getFuelType() {
+			return fuelType;
+		}
+		
+		public String getDecalColour() {
+			return decalColour;
+		}
+
+		@Override
+		public String toString() {
+			return   "\"LicensePlatePK\" : \""+ year + "\" , \"manufacturer\" : \"" + manufacturer + "\" , \"makeAndModel\" : \""+ makeAndModel + "\" , \"exhaustStandard\" : \""+ exhaustStandard + "\" , \"fuelType\" : \""+ fuelType + "\" , \"decalColour\" : \"" + decalColour+"\"";
+		}
+
+	}
+  
+ 
+  private static List<VehicleSpecies> readModelsFromCSV(String fileName) {
+	  //System.out.println("in readModelsFromCSV fileName >>>> "+fileName);
+	  List<VehicleSpecies> vehicles = new ArrayList<>(); 
+	  Path pathToFile = Paths.get(fileName); 
+	  // create an instance of BufferedReader 
+	  // using try with resource, Java 7 feature to close resources 
+	  try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) 
+	 //try (BufferedReader br = Files.newBufferedReader(pathToFile, Charset.forName("UTF-8")) 
+	  { 
+	  // read the first line from the text file 
+	  String line = br.readLine();
+	  // loop until all lines are read 
+	  //System.out.println("in try readModelCSV line >>>> "+line);
+	  while (line != null) { 
+	  // use string.split to load a string array with the values from 
+	  // each line of 
+	  // the file, using a comma as the delimiter 
+	  String[] attributes = line.split(",");
+	  //System.out.println("in try readModelCSV while attributes[0] >>>> "+attributes[0]);
+	  VehicleSpecies vehicleSpecies = createVehicleSpecies(attributes); 
+	  // adding book into ArrayList 
+	  vehicles.add(vehicleSpecies); 
+	  // read next line before looping 
+	  // if end of file reached, line would be null 
+	  line = br.readLine(); 
+	  }
+	  } 
+	
+      catch (Exception e) {
+		  System.out.println(" Exception >> "+e);
+		  e.printStackTrace(); 
+		  } 
+
+        //catch (MalformedInputException mie) {
+		//  System.out.println(" MalformedInputException >> "+mie);
+		 // mie.printStackTrace(); 
+		 // }		  
+    return vehicles; 
+    }
+	
+	
+	private static VehicleSpecies createVehicleSpecies(String[] metadata) {
+		
+		int year = Integer.parseInt(metadata[0]); 
+		String manufacturer = metadata[1] ;
+		String makeAndModel= metadata[2] ;
+		String exhaustStandard= metadata[3] ;
+		String fuelType= metadata[4] ;
+		String decalColour= metadata[5] ;
+		// create and return book of this metadata 
+		return new VehicleSpecies(year, manufacturer, makeAndModel, exhaustStandard ,fuelType , decalColour); 
+		}
+	
+	
+	/**
+	 * @param licensePlateNumber
+	 * @return
+	 */
+	private static VehicleSpecies getVehicleSpecies(String licensePlateNumber) {
+		// gwt same make model for the given licenseplate if repeats
+		try{
+			int bucket = licensePlateNumber.hashCode() % vehiclesSpeciesList.size();
+		VehicleSpecies species = vehiclesSpeciesList.get(bucket);
+		return species;
+			
+		}catch(Exception e){
+			System.out.println("L P N >>> "+licensePlateNumber.hashCode()+" size >>> " + vehiclesSpeciesList.size());
+			
+		}	
+	return null;	
+	}
+  
+ }
