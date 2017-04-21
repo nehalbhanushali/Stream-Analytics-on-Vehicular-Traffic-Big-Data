de239ab7e682935b64d6fa2b61fdbac2853a0138
rutikaranjolkar <ranjolkar.r@husky.neu.edu>
2017-04-21 13:25:13 -0400

    Add files via upload
    New working shell file

--- Step1/fwdvidmapred/vidmapred.sh
+++ Step1/fwdvidmapred/vidmapred.sh
@@ -1,5 +1,7 @@
 SCRIPT_FILE=$(readlink -f "$0")
 BASEDIR=$(dirname "$SCRIPT_FILE")
+# above 2 lines is a hack to get current folder
+
 HADOOP_HOME="/usr/local/hadoop-2.8.0"
 echo "The Hadoop home is set to $HADOOP_HOME"
 
@@ -32,7 +34,16 @@ mkdir -p $MAPRED_OUT_DIR
 rm -r $MAPRED_OUT_DIR
 $HADOOP_HOME/bin/hadoop jar $HADOOP_STRM_JAR -input $MAPRED_INP_DIR -output $MAPRED_OUT_DIR -mapper $BASEDIR/mapper.py -reducer $BASEDIR/reducer.py
 
+echo "******************"
+echo "******************"
+echo "******************"
+echo "******************"
+
 echo "Finished python map/reduce! The output is available in $MAPRED_OUT_DIR"
+echo "******************"
+echo "******************"
+echo "******************"
+echo "******************"
 
 JAVAMAPRED_CLASSES_DIR="$BASEDIR/license_classes"
 
@@ -40,10 +51,14 @@ mkdir -p $JAVAMAPRED_CLASSES_DIR
 rm -r $JAVAMAPRED_CLASSES_DIR
 mkdir -p $JAVAMAPRED_CLASSES_DIR
 
-javac -classpath $HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.8.0.jar -d $JAVAMAPRED_CLASSES_DIR $BASEDIR/LicenseMapper1.java $BASEDIR/LicenseReducer1.java $BASEDIR/LicenseDriver1.java
+echo "%%%%%%%%%%%%%%%%%%%%%%"
+echo "%%%%%%%%%%%%%%%%%%%%%%"
+
+javac -classpath $HADOOP_HOME/share/hadoop/common/hadoop-common-2.8.0.jar:$HADOOP_HOME/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.8.0.jar -d $JAVAMAPRED_CLASSES_DIR $BASEDIR/LicenseMapper1.java $BASEDIR/LicenseReducer1.java $BASEDIR/LicenseDriver1.java
 
 jar cvf $BASEDIR/license.jar -C $JAVAMAPRED_CLASSES_DIR/ .
+rm -r $BASEDIR/licenselist_output_mm
 
-$HADOOP_HOME/bin/hadoop jar $BASEDIR/license.jar $BASEDIR/U.CC.LicenseDriver1 $MAPRED_OUT_DIR $BASEDIR/licenselist_output_mm
+$HADOOP_HOME/bin/hadoop jar $BASEDIR/license.jar U.CC.LicenseDriver1 $MAPRED_OUT_DIR $BASEDIR/licenselist_output_mm
 
 echo "Finished java map/reduce! The output is available in $BASEDIR/licenselist_output_mm"
