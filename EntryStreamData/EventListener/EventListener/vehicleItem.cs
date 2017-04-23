72c5a47bac2899627c5e2b8b04eb9637a681996e
amithamurali <murali.a@husky.neu.edu>
2017-04-23 14:38:55 -0400

    Add files via upload

---
+++ EntryStreamData/EventListener/EventListener/vehicleItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventListener
+{
+    class VehicleItem
+    {
+        public string Manufacturer;
+        public string MakeAndModel;        
+        public string LicensePlatePK;
+        public string LicensePlateRK;
+        public string Year;
+        public string LicensePlate;
+        public string FuelType;
+        public string City;
+        public string lat;
+        public string lng;
+        public DateTime datetime;
+
+    }
+}
