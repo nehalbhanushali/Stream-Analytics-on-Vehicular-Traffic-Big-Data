a48818f3b987472023dae9953953e7e3b85651b0
amithamurali <murali.a@husky.neu.edu>
2017-04-23 14:52:14 -0400

    Add files via upload

---
+++ CAV tags/EventListener/EventListener/vehicleItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAVTagsDataEventListener
+{
+    class VehicleItem
+    {
+        public string Manufacturer;
+        public string MakeAndModel;        
+        public string LicensePlatePK;
+        public string LicensePlateRK;
+        public string ExhaustStandard;
+        public string FuelType;
+        public string Rating;
+        public string MPG;
+        public string CO2;
+        public string City;
+        public string DecalColour;
+        public DateTime datetime;
+
+    }
+}
